b4f591f1e196c4e9b2a6199b84560ff62d3c8ede
momo <ptjkjm1@gmail.com>
2026-08-12 18:45:03 +0900

    init commit

---
+++ src/main/java/io/github/spartateam6/commercepaymentsystem/CommercePaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package io.github.spartateam6.commercepaymentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercePaymentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommercePaymentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/spartateam6/commercepaymentsystem/CommercePaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.spartateam6.commercepaymentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercePaymentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
