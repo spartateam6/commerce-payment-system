a37f0d24077dee6b70324a23b5dbec3b08fddf67
trex1004 <trexs@gmail.com>
2026-08-15 18:17:31 +0900

    test: AuthService 로그인시 비밀번호 불일치 테스트 추가

--- src/test/java/io/github/spartateam6/commercepaymentsystem/domain/member/service/AuthServiceTest.java
+++ src/test/java/io/github/spartateam6/commercepaymentsystem/domain/member/service/AuthServiceTest.java
@@ -75,7 +75,7 @@ class AuthServiceTest {
     }
 
     @Test
-    void signUp_이메일이_중복이면_예외를_던진다(){
+    void signUp_이메일이_중복이면_예외를_던진다() {
 
         //given
         SignUpRequest request = new SignUpRequest(
@@ -99,7 +99,7 @@ class AuthServiceTest {
     }
 
     @Test
-    void login_성공하면_토큰_발급한다() {
+    void Login_성공하면_토큰_발급한다() {
         // given
         LoginRequest request = new LoginRequest(
                 "trexs1004@gmail.com",
@@ -116,10 +116,10 @@ class AuthServiceTest {
                 .willReturn(Optional.of(member));
 
         // 비밀번호 일치 상황 설정
-        given(passwordEncoder.matches(request.getPassword(),member.getPassword()))
+        given(passwordEncoder.matches(request.getPassword(), member.getPassword()))
                 .willReturn(true);
 
-       // JWT 생성결과 설정
+        // JWT 생성결과 설정
         given(jwtProvider.createToken(member.getId())).willReturn("fake-jwt-token");
 
         // when
@@ -127,10 +127,75 @@ class AuthServiceTest {
         LoginResponse response = authService.login(request);
 
         //then
+        // 비밀번호 검증이 호출됐는지 검증
+        verify(passwordEncoder)
+                .matches(request.getPassword(), member.getPassword());
+
         // JWT 생성이 호출됐는지 검증
         verify(jwtProvider).createToken(member.getId());
 
         //발급된 토큰 검증
         assertThat(response.getAccessToken()).isEqualTo("fake-jwt-token");
     }
+
+    @Test
+    void Login_비민번호가_틀리면_예외_던진다() {
+        // given
+        LoginRequest request = new LoginRequest(
+                "trexs1004@gmail.com",
+                "qwer1234@"
+        );
+
+        Member member = Member.builder()
+                .id(1L)
+                .email(request.getEmail())
+                .password("encoded-password")
+                .build();
+
+        // 이메일로 회원조회시 회원 존재상황 설정
+        given(memberRepository.findByEmail(request.getEmail()))
+                .willReturn(Optional.of(member));
+
+        // 입력한 비밀번호가 저장된 비밀번호와 일치하지 않은 상황 설정
+        given(passwordEncoder.matches(
+                request.getPassword(),
+                member.getPassword()
+        )).willReturn(false);
+
+        // when & then
+        // 비밀번호가 일치하지 않으면 예외 발생하는지 검증
+        assertThatThrownBy(() -> authService.login(request))
+                .isInstanceOf(BusinessException.class)
+                .hasMessage(ErrorCode.INVALID_CREDENTIALS.getMessage());
+
+
+        // 비밀번호 불일치시 JWT 발급 되지 않아야 한다..
+        verify(jwtProvider, never()).createToken(any());
+    }
+
+    @Test
+    void Login_회원이_없으면_예외_던짐() {
+
+        //given
+        LoginRequest request = new LoginRequest(
+                "unkown@unkown.com",
+                "qwer1234@"
+        );
+
+        // 이메일로 회원 조회 했는데 회원 없음;;
+        given(memberRepository.findByEmail(request.getEmail()))
+                .willReturn(Optional.empty());
+
+        // when & then
+        assertThatThrownBy(() -> authService.login(request))
+                .isInstanceOf(BusinessException.class)
+                .hasMessage(ErrorCode.INVALID_CREDENTIALS.getMessage());
+
+        // 없으면 비밀번호 검증 및 JWT 발급호출 X
+        verify(passwordEncoder, never()).matches(any(), any());
+        verify(jwtProvider, never()).createToken(any());
+
+    }
+
+
 }
