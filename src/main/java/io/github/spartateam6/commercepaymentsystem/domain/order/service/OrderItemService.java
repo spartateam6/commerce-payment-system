d9973038eebc79360ac1281e03dc29190ebd3e09
momo <ptjkjm1@gmail.com>
2026-08-17 05:43:53 +0900

    feat: 주문 상품 재고 복원 로직 추가

---
+++ src/main/java/io/github/spartateam6/commercepaymentsystem/domain/order/service/OrderItemService.java
@@ -0,0 +1,31 @@
+package io.github.spartateam6.commercepaymentsystem.domain.order.service;
+
+import io.github.spartateam6.commercepaymentsystem.domain.order.entity.OrderItem;
+import io.github.spartateam6.commercepaymentsystem.domain.order.repository.OrderItemRepository;
+import io.github.spartateam6.commercepaymentsystem.domain.product.entity.Product;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class OrderItemService {
+
+    private final OrderItemRepository orderItemRepository;
+
+    @Transactional
+    public void restoreOrderProductStock(Long orderId) {
+        List<OrderItem> orderItemList = orderItemRepository.findByOrder_Id(orderId);
+
+        for (OrderItem orderItem : orderItemList) {
+            Product product = orderItem.getProduct();
+            Integer stock = orderItem.getQuantity();
+
+            product.addStock(stock);
+        }
+    }
+
+}
