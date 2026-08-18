f0b5adc1da81bc948915260a4f7244ecdc57f184
trex1004 <trexs@gmail.com>
2026-08-18 11:46:46 +0900

    refactor: SignUpRequest

--- src/main/java/io/github/spartateam6/commercepaymentsystem/domain/member/dto/SignUpRequest.java
+++ src/main/java/io/github/spartateam6/commercepaymentsystem/domain/member/dto/SignUpRequest.java
@@ -5,28 +5,27 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class SignUpRequest {
 
-    @NotNull
-    @Email
-    @Size(max = 255)
-    private String email;
+public record SignUpRequest(
 
-    @NotBlank
-    @Pattern(
-            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[^A-Za-z0-9]).{8,}$",
-            message = "비밀번호는 영문, 숫자, 특수문자를 포함해 8자 이상이어야 합니다"
-    )
-    private String password;
+        @NotNull
+        @Email
+        @Size(max = 255)
+        String email,
 
-    @NotBlank
-    private String name;
+        @NotBlank
+        @Pattern(
+                regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[^A-Za-z0-9]).{8,}$",
+                message = "비밀번호는 영문, 숫자, 특수문자를 포함해 8자 이상이어야 합니다"
+        )
+        String password,
 
-    @NotBlank
-    @Pattern(regexp = "^010-\\d{4}-\\d{4}$",
-            message = "전화번호는 010-0000-0000 형식이어야 합니다")
-    private String phoneNumber;
+        @NotBlank
+        String name,
+
+        @NotBlank
+        @Pattern(regexp = "^010-\\d{4}-\\d{4}$",
+                message = "전화번호는 010-0000-0000 형식이어야 합니다")
+        String phoneNumber
+) {
 }
