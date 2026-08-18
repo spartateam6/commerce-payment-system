e3fdacd6fcf32c29b72edadfd05a35237a6779cf
trex1004 <trexs@gmail.com>
2026-08-19 03:24:37 +0900

    refactor: OrderService.getOrder/  OrderService.getOrderByOrderNumber-  주문 상세조회 N+1 ,403예외

--- src/main/java/io/github/spartateam6/commercepaymentsystem/domain/order/repository/OrderRepository.java
+++ src/main/java/io/github/spartateam6/commercepaymentsystem/domain/order/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package io.github.spartateam6.commercepaymentsystem.domain.order.repository;
 
 import io.github.spartateam6.commercepaymentsystem.domain.order.entity.Order;
+import org.jspecify.annotations.NonNull;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,8 +11,9 @@ import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+    @NonNull
     @EntityGraph(attributePaths = {"orderItems", "orderItems.product"})
-    Optional<Order> findById(Long id);
+    Optional<Order> findById(@NonNull Long id);
 
     Optional<Order> findByOrderNumber(String orderNumber);
 
