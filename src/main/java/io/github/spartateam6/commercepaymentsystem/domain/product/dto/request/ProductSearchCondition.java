12d4a930b17456265f18472c933fadb0ebbc4502
kimbomin <bomin0322s2@gmail.com>
2026-08-20 12:39:08 +0900

    feat:ProductSearchCondition에 saleStatus, soldOut, sort 필드 추가

--- src/main/java/io/github/spartateam6/commercepaymentsystem/domain/product/dto/request/ProductSearchCondition.java
+++ src/main/java/io/github/spartateam6/commercepaymentsystem/domain/product/dto/request/ProductSearchCondition.java
@@ -1,9 +1,14 @@
 package io.github.spartateam6.commercepaymentsystem.domain.product.dto.request;
 
+import io.github.spartateam6.commercepaymentsystem.domain.product.entity.SaleStatus;
+
 public record ProductSearchCondition(
         String category,
         Integer minPrice,
         Integer maxPrice,
+        SaleStatus saleStatus,
+        Boolean soldOut,
+        String sort,
         int page,
         int size
 ){}
