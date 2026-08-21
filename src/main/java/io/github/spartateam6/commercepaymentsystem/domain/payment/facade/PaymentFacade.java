759ad6c1519d9e0cce4d8570fd850c66ff18cb0e
trex1004 <trexs@gmail.com>
2026-08-21 17:47:14 +0900

    fix: PaymentFacade.confirmPayment 분기추가

--- src/main/java/io/github/spartateam6/commercepaymentsystem/domain/payment/facade/PaymentFacade.java
+++ src/main/java/io/github/spartateam6/commercepaymentsystem/domain/payment/facade/PaymentFacade.java
@@ -27,6 +27,13 @@ public class PaymentFacade {
     ) {
         PaymentDto paymentDto = paymentService.validPayment(memberId, paymentRequestDto);
 
+        // 포인트로 전액 결제하면 PG를 안거치도록한다.
+        if (paymentDto.pgAmount() == 0){
+            paymentService.successPayment(memberId, paymentRequestDto.orderNumber(), 0L);
+
+            return PaymentConfirmResponseDto.success(paymentDto);
+        }
+
         PaymentGatewayResponse pgPayment = paymentGateway.getPayment(paymentDto.portonePaymentId());
 
         boolean isPaid = pgPayment.isPaid();
