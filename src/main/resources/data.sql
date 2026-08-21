44ef6ff88ac422b00b2d983cc7b7081b99af46f7
trex1004 <trexs@gmail.com>
2026-08-21 18:01:54 +0900

    chore : 포인트 전액 결제 테스트용 더미 회원 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -48,7 +48,18 @@ VALUES
         '$2a$12$GeifU8Kqme5IKtQa9aVAN.CWXCRjHNwIlYssY64Hw.VCBpDnu6wky',
         '010-2222-2222',
         0
-    );
+    ),
+    (
+        3,
+        NOW(6),
+        NOW(6),
+        'user@example.com',
+        '포인트많은유저',
+        '$2a$12$GeifU8Kqme5IKtQa9aVAN.CWXCRjHNwIlYssY64Hw.VCBpDnu6wky',
+        '010-3333-3333',
+        1000000
+    )
+;
 
 
 -- ---------------------------------------------------------
@@ -277,4 +288,5 @@ VALUES
 INSERT INTO point_transactions (member_id, payment_id, transaction_type, amount, created_at, updated_at)
 VALUES (1, NULL, 'EARN', 10000, NOW(6), NOW(6)),
        (1, 2, 'USE', -5000, NOW(6), NOW(6)),
-       (1, 2, 'EARN', 250, NOW(6), NOW(6));
+       (1, 2, 'EARN', 250, NOW(6), NOW(6)),
+       (3, NULL, 'EARN', 1000000, NOW(6), NOW(6));
